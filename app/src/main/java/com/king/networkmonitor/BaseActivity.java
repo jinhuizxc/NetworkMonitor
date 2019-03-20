0e99d701667feb09d5707ce3f7172b2d3a5dce24
j_h <13113017500>
2019-03-20 18:36:37 +0800

    补充说明;

--- app/src/main/java/com/king/networkmonitor/BaseActivity.java
+++ app/src/main/java/com/king/networkmonitor/BaseActivity.java
@@ -7,6 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 /**
  * <请描述这个类是干什么的>
  * Created by wwb on 2017/7/13 17:57.
+ * 
+ *  使用方式: 在BaseActivity里面
+ *  onCreate()初始化:  NetworkReceiver.addCallback(this);
+ *  onDestory()销毁时:  NetworkReceiver.removeCallback(this);
+ *  在需要使用的地方接收回调方法参数即可。
  */
 
 public class BaseActivity extends AppCompatActivity implements NetworkReceiver.NetworkCallback {
